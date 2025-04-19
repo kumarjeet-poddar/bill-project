f7a1942b9a5fc3e69311bf773dabd6d2b4e9d6ef
Sanika goyal <sanika2110118@akgec.ac.in>
2025-04-19 18:26:27 +0530

    changed backend url

--- frontend/src/Utils/axiosInstance.js
+++ frontend/src/Utils/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://bill-project-backend.onrender.com/',
-  // baseURL: 'http://localhost:3002/',
+  // baseURL: 'https://bill-project-backend.onrender.com/',
+  baseURL: 'https://bill-project-r2q4.onrender.com/',
 });
 
 export default axiosInstance;
